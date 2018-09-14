e7d893e4b2e2661eb41b215eda3557bfcdf45b11
jldeen <jessde@microsoft.com>
2018-09-14 14:13:31 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "./wiring.h"
+
+const int MAX_BLINK_TIMES = 20;
+const int GREEN_LED_PIN = 7;
+const int BLUE_LED_PIN = 22;
+
+int main(int argc, char *argv[])
+{
+    int blinkNumber = 0;
+
+    setupWiringPi();
+    setPinMode(BLUE_LED_PIN, OUTPUT);
+    setPinMode(GREEN_LED_PIN, OUTPUT);
+
+    while (MAX_BLINK_TIMES > blinkNumber++)
+    {
+        printf("[Device] #%d Blue LED \n", blinkNumber);
+        writeToPin(BLUE_LED_PIN, HIGH);
+        wait(500);
+        writeToPin(BLUE_LED_PIN, LOW);
+        printf("[Device] #%d Green LED \n", blinkNumber);
+        writeToPin(GREEN_LED_PIN, HIGH);
+        wait(500);
+        writeToPin(GREEN_LED_PIN, LOW);
+    }
+
+    return 0;
+}
